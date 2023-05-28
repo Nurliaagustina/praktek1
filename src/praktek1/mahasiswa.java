1921ce80f9783c11325c1ee2016e5b57abfdeb34
Nurlia Agustina <liaagustina.app@gmail.com>
2023-05-28 12:43:44 +0800

    Membuat atribut dan behavior

--- src/praktek1/mahasiswa.java
+++ src/praktek1/mahasiswa.java
@@ -9,5 +9,14 @@ package praktek1;
  * @author ASUS
  */
 public class mahasiswa {
-    
+   string nama;
+   string NPM;
+   double IPK;
+
+   
+   void cetakinfo(){
+       system.out.println("nama :"+nama);
+       system.out.println("NPM  :"+NPM);
+       system.out.println("IPK  :"+IPK);
+   }
 }
